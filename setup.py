d991b8199641f2e3f81a03b0508f6e7001820025
Jasmine Ratchford <jratchford@sei.cmu.edu>
2024-11-17 13:25:15 -0500

    update structure

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='bs_net',
-   version='0.0.1',
+   name='bsnet',
    description='bspline_net',
-   author='Jacob Wang',
-   author_email='',
-   package_dir={"": "src"},
-   packages=find_packages(where="src", include=["src*"]), 
-   install_requires=['matplotlib', 'numpy', 'scipy','fire'], #external packages as dependencies
+   author='Jasmine Ratchford',
+   author_email='jratchford@sei.cmu.edu',
+   version='0.1.0',
+   package_dir={'': 'src'},
+   packages=find_packages(where='src'),
+   install_requires=['matplotlib', 'numpy', 'scipy','fire', 'torch', 'tensorboard', 'lightning', 'opt-einsum','jsonargparse[signatures]>=4.27.7'], #external packages as dependencies
 )
